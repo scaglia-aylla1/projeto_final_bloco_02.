7ecd295d5b9b91d5d560a769e4360d40b6d11588
Aylla Scaglia <aylla@scaglia.com.br>
2025-05-28 11:14:38 -0300

    :sparkles: Model/Repository/Controller Categoria

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,58 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+
+	private String descricao;
+
+//	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+//	@JsonIgnoreProperties("categoria")
+//	private List<Produto> produtos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+//	public List<Produto> getProdutos() {
+//		return produtos;
+//	}
+//
+//	public void setProdutos(List<Produto> produtos) {
+//		this.produtos = produtos;
+//	}
+
+	
+}
---
+++ src/main/java/com/generation/farmacia/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.generation.farmacia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.farmacia.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
